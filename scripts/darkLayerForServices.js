aa2f94f6b3e4346f460e9bf5ebd874f0afe78d3f
Your Name <your_email@whatever.com>
2021-04-06 19:43:58 +0300

    menu for all pages

--- scripts/darkLayerForServices.js
+++
@@ -1,6 +0,0 @@
-function darkLayerAdd() {
-  var servicesItems = document.getElementsByClassName('services__item_layer');
-  servicesItems[onmouseover].style.display = 'block';
-  servicesItems[onmouseout].style.display = 'none';
-  }
-  
