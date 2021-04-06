fa56844d3001ed7cd7246477e51be8087b1a695b
Your Name <your_email@whatever.com>
2021-04-06 18:28:56 +0300

    commir for color

---
+++ scripts/darkLayerFor Services.js
@@ -0,0 +1,12 @@
+function darkLayerAdd() {
+  var servicesItems = document.getElementsByClassName('services__item_layer');
+  servicesItems[onmouseover].style.display = 'block';
+  servicesItems[onmouseout].style.display = 'none';
+  }
+
+  function textAdd() {
+    var textItems = document.getElementsByClassName('services__text');
+    textItems[onmouseover].style.display = 'block';
+    textItems[onmouseout].style.display = 'none';
+    }
+  
---
+++ scripts/darkLayerForServices.js
@@ -0,0 +1,6 @@
+function darkLayerAdd() {
+  var servicesItems = document.getElementsByClassName('services__item_layer');
+  servicesItems[onmouseover].style.display = 'block';
+  servicesItems[onmouseout].style.display = 'none';
+  }
+  
